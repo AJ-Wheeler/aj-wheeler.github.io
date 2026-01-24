da9c2900fe6b9959cbb6096d8e6139df03c1588c
AJ-Wheeler <128074031+AJ-Wheeler@users.noreply.github.com>
2026-01-24 03:36:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -542,14 +542,15 @@ case "lewis":
     type: "ascii+html",
     ascii: ASCII_ART.lewis,
     html: `
-      <br><b>*** CREW MEMBER: LEWIS ***</b><br><br>Oxygen Levels: Poor<br>C02 Levels: Poor<br>H20 Levels: Poor<br>Blood Pressure: !HIGH!<br>Heart Rate: 77`
+      <br><b>*** CREW MEMBER: LEWIS ***</b><br><br>Oxygen Levels: Poor<br>C02 Levels: Poor<br>H20 Levels: Poor<br>Blood Pressure: !HIGH!<br>Heart Rate: 77<br><br>Assignment: Eliminating all foreign organic waste`
   };
 
       case "callie":
         return {
-          type: "html",
-          header: "",
-          content: `<br><b>*** CREW MEMBER: CALLIE ***</b><br><br>Oxygen Levels: Good<br>C02 Levels: Good<br>H20 Levels: Excellent<br>Blood Pressure: Fair<br>Heart Rate: 68`
+          type: "ascii+html",
+          ascii: ASCII_ART.callie,
+          html: `
+          <br><b>*** CREW MEMBER: CALLIE ***</b><br><br>Oxygen Levels: Good<br>C02 Levels: Good<br>H20 Levels: Excellent<br>Blood Pressure: Fair<br>Heart Rate: 68<br><br>Assignment: Supervising all lower-level employees`
   };
       case "calliefornia":
         return ASCII_ART.callie;
