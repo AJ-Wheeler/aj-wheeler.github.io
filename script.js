b0ccbb05272d53c01cba61d5b3dcf67840e62c88
AJ-Wheeler <128074031+AJ-Wheeler@users.noreply.github.com>
2026-01-24 21:57:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -311,7 +311,7 @@ const htmlToTerminalText = (html) => {
 
   // --- Communications Logs ---
 const commsLogs = {
-  "LOG-001": "[INBOUND]\nFrom: ADM ÂCKB░R \nStatus: CU+00D8RR░PTΞD\nMessage: ...IT'S Â TR░P-",
+  "LOG-001": "[INBOUND]\nFrom: ADM ÂCKB░R \nStatus: C█RR░PTΞD\nMessage: ...IT'S Â TR░P-",
   "LOG-002": "[OUTBOUND]\nTo: Vessel ATLAS-9\nStatus: SENT\nMessage: Approaching rendezvous point.\nSignal strength stable.",
   "LOG-003": "[INBOUND]\nFrom: Vessel ATLAS-9\nStatus: RECEIVED\nMessage: Acknowledged. Holding position.",
   "LOG-004": "[OUTBOUND]\nTo: Mission Control\nStatus: FAILED\nMessage: Navigation anomaly detected. Requesting guidance."
